cb73d20f734cf35998d034bd5a2f652cdf652e61
Alan <Alan@dev.com>
2022-11-07 18:18:27 -0600

    Final Version.

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Alan Macías</p>
+        <p>Alan Macías Reta - CICD Course QA</p>
         <a
           className="App-link"
           href="https://reactjs.org"
